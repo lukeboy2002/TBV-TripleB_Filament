939ce402d6c24d5c998f7d13f06c9eb1b9b39961
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2023-12-08 19:17:50 +0100

    changed model
    included HasMedia

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Member extends Model
+class Member extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
+
 
     protected $fillable = [
         'id',
@@ -27,5 +33,11 @@ class Member extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->fit(Manipulations::FIT_MAX, 1000, 1000);
     }
 }
