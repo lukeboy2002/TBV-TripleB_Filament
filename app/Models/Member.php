33bce9f765eacb8a15d5101fc5b2c562c3a8ff96
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2023-12-08 19:11:55 +0100

    created model
    migration
    seeders
    Installation spatie medialibrary

---
+++ app/Models/Member.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Member extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'image',
+        'bio',
+        'city',
+        'birthdate',
+        'points',
+        'played_games',
+        'won_games',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
